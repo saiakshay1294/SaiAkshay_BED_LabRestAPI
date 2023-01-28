ee98a31b68030e3296de8f6eb16ff9e7806578be
pradeep <pradeep.kumar44@gmail.com>
2023-01-28 19:06:15 +0530

    committing the changes

--- src/main/java/com/glearning/students/config/BootstrapAppData.java
+++ src/main/java/com/glearning/students/config/BootstrapAppData.java
@@ -6,6 +6,7 @@ import org.springframework.context.event.EventListener;
 
 import com.glearning.students.dao.StudentRepository;
 import com.glearning.students.dao.UserRepository;
+import com.glearning.students.model.Student;
 
 import lombok.RequiredArgsConstructor;
 
@@ -21,6 +22,15 @@ public class BootstrapAppData {
 	@EventListener(ApplicationReadyEvent.class)
 	public void initializeData(ApplicationReadyEvent readyEvent) {
 		
+		Student ravi = new Student("Ravi", "Kumar", "Java", "India");
+		Student harish = new Student("Harish", "Prasad", "Python", "India");
+		Student ramesh = new Student("Ramesh", "Patil", "NodeJs", "India");
+		Student krishna = new Student("Krishna", "Patel", "Java", "India");
+		
+		this.studentRepository.save(ravi);
+		this.studentRepository.save(harish);
+		this.studentRepository.save(ramesh);
+		this.studentRepository.save(krishna);
 		
 		
 	}
